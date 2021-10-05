f01daeb32718c4bf25123aab69b2056c7bcdb550
heserna <56064774+heserna@users.noreply.github.com>
2021-10-05 13:15:58 +0200

    Add files via upload

---
+++ src/ejerciciodivisas/Conversor.java
@@ -0,0 +1,30 @@
+package ejerciciodivisas;
+import java.util.*;
+
+public class Conversor {
+public Divisa dref;
+protected Map<String , Divisa>  ColeccionDivisas;
+
+public Conversor(Divisa d) {
+	this.dref=d;
+	this.ColeccionDivisas= new HashMap<>();
+	this.ColeccionDivisas.put( d.getNombre(),d);
+}
+
+public void Ampliarcoleccion(Divisa d) {
+	this.ColeccionDivisas.put( d.getNombre(),d);
+}
+
+public double Convertir(Divisa d1, Divisa d2, double cantidad) {
+	if (d1.mismadivisa(d2)) return cantidad;
+	else return (cantidad*d1.getValor())/d2.getValor();
+}
+
+public boolean ExisteDivisa( Divisa d) {
+	return this.ColeccionDivisas.containsKey(d.getNombre());
+}
+
+
+	
+}
+
